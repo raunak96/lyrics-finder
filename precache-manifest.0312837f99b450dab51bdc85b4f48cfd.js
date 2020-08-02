bcb6fc63baebad44a82ccccc11e08d2eaf568b61
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-08-03 00:55:54 +0530

    Updates

---
+++ precache-manifest.0312837f99b450dab51bdc85b4f48cfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f0dad485d720dcbf986eaee821c997",
+    "url": "/lyrics-finder/index.html"
+  },
+  {
+    "revision": "c88b74eb97ffd5c8fa64",
+    "url": "/lyrics-finder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "891224f4af8668e747aa",
+    "url": "/lyrics-finder/static/js/2.d5454cf7.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/lyrics-finder/static/js/2.d5454cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88b74eb97ffd5c8fa64",
+    "url": "/lyrics-finder/static/js/main.df0cb5fb.chunk.js"
+  },
+  {
+    "revision": "f8f1577f707650a872ab",
+    "url": "/lyrics-finder/static/js/runtime-main.4daa7b91.js"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/lyrics-finder/static/media/spinner.235b9b3e.gif"
+  }
+]);
